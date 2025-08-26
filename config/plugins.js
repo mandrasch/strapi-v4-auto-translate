17635aab6101195f53f6497de57a77b2cb261c8b
Matthias Andrasch <matthias.andrasch@mailbox.org>
2025-08-26 12:20:41 +0200

    chore: config for translate, use deepl

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,17 @@
-module.exports = () => ({});
+module.exports = () => ({
+   translate: {
+    enabled: true,
+    config: {
+      provider: 'deepl',
+      providerOptions: {
+        localeMap: {
+          EN: 'EN-US',
+        },
+        apiOptions: {
+          formality: 'default',
+          preserveFormatting: true,
+        },
+      },
+    },
+  },
+});
